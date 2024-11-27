8e8799a69278b0738b2f1ceca6812376ff158d1f
ShrutiSantoshShinde <167123141+ShrutiSantoshShinde@users.noreply.github.com>
2024-11-27 23:44:31 +0530

    Add files via upload

---
+++ Book_Mangement_System/Book.java
@@ -0,0 +1,47 @@
+package Book_Mangement_System;
+
+public class Book {
+    public int book_id;
+    public String title, author, publisher;
+    public Double price;
+
+    public int getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(int book_id) {
+        this.book_id = book_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
---
+++ Book_Mangement_System/Main.java
@@ -0,0 +1,7 @@
+package Book_Mangement_System;
+
+public class Main {
+    public static void main(String []args){
+        Book_Frame b=new Book_Frame();
+    }
+}
